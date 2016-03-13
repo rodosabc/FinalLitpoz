8a7a5a8f458a21ca312d8d94f1d6e3db42221f5a
rodosabc <rodosabc@gmail.com>
2016-03-13 05:35:28 +0300

    Added files via upload
    ИЛЮША НАПИСАЛ СКРИПТ

--- app/assets/javascripts/scripts.js
+++ app/assets/javascripts/scripts.js
@@ -1,3 +1,8 @@
-$(document).on('page:load', function() {
-    $(".icon-menu").click(function(t){ $(".right_menu").toggleClass("active")
-    })});
+window.onload = function(){
+    var menu = document.getElementsByClassName('right_menu')[0];
+    var btn = document.getElementsByClassName('icon-menu')[0];
+
+    btn.addEventListener('click', function(){
+        menu.classList.toggle('active');
+    })
+}
