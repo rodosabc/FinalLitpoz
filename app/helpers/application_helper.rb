f3a48decb49ac753247357d4289aa2c21f0cfe0d
rodosabc <rodosabc@gmail.com>
2016-03-15 06:01:23 +0300

    Added metatags content, removed stupid passwords in /admin

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,11 @@
 module ApplicationHelper
   def title(page_title)
     content_for :title, page_title.to_s
+    end
+  def description(page_description)
+    content_for :description, page_description.to_s
+  end
+  def keywords(page_keywords)
+    content_for :keywords, page_keywords.to_s
   end
 end
