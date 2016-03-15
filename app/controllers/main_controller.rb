e933df0c620f3e2cac129355d4d6bd15e6d9c11f
rodosabc <rodosabc@gmail.com>
2016-03-15 16:39:50 +0300

    Added print poems for current holiday on main page

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,5 +1,12 @@
 class MainController < ApplicationController
   def index
-    ""
+    @holidays.each do |holiday|
+      current_date = (Time.now.month.to_s + Time.now.day.to_s).to_i
+      if current_date <= holiday.date
+        @holiday = holiday
+        @poems = @holiday.poems.page(params[:page]).per_page(5)
+      end
+    end
+
   end
 end
